ad90c7a32ca4ee05346f2e0eae06af8975d6ff61
emmaagbeso <emmaagbeso3@gmail.com>
2026-05-16 14:02:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const CLIENTS = {
   "1021390397733796": {
